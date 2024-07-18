23cba69564ecdedb1828c1870f2d0b2689ea9065
ninnistic <104331470+ninnistic@users.noreply.github.com>
2024-07-18 22:10:25 +0900

    create tooltips w/ mousehover

--- components/maps/map.tsx
+++ components/maps/map.tsx
@@ -8,29 +8,63 @@ import {
   Graticule,
 } from 'react-simple-maps';
 import geoURL from './world.json';
+import { Tooltip, TooltipRefProps } from 'react-tooltip';
+import { useRef, useId } from 'react';
 
 export default function Map() {
-  //   const geoURL = 'https://unpkg.com/world-atlas@2.0.2/countries-110m.json';
-  //   const geoURL = './features.json';
+  let tooltip = useRef<TooltipRefProps>(null);
+  const id = useId();
   return (
-    <div className="bg-red-400">
-      <ComposableMap>
-        <ZoomableGroup
-          center={[80, 10]}
-          fill="white"
-          stroke="black"
-          strokeWidth={0.5}
-          zoom={1.5}
-        >
+    <div className="bg-red-400 relative">
+      <ComposableMap
+        projection="geoMercator"
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+      >
+        <ZoomableGroup>
+          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
           <Geographies geography={geoURL}>
             {({ geographies }) =>
               geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
+                <Geography
+                  key={geo.rsmKey}
+                  onMouseEnter={() => {
+                    document.body.classList.add('target');
+                  }}
+                  onMouseLeave={() => {
+                    document.body.classList.remove('target');
+                  }}
+                  geography={geo}
+                  stroke="#bec1c4"
+                  fill="white"
+                  strokeWidth="0.5"
+                  className="focus:outline-none"
+                  data-tooltip-id={id}
+                />
               ))
             }
           </Geographies>
         </ZoomableGroup>
       </ComposableMap>
+      <Tooltip
+        ref={tooltip}
+        id={id}
+        float
+        className={`!p-0 !rounded-lg !bg-white !text-inherit select-none z-10`}
+        opacity={1}
+      >
+        <div className="flex gap-x-5">
+          <div className="flex justify-center items-center">
+            <p className="font-primary-Regular font-bold">Brazil</p>
+          </div>
+          <div>
+            <small className=" font-primary-Regular">Online Revenue</small>
+            <p className="text-sm font-primary-Regular">$13,450,000</p>
+          </div>
+        </div>
+      </Tooltip>
     </div>
   );
 }
